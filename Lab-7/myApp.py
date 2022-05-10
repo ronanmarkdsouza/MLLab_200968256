588d94097e3ffbc39a52bb00becfc06c39b863e8
Ronan Mark Dsouza <ronanmarkdsouza@gmail.com>
2022-05-10 16:52:45 +0530

    myApp.py

--- Lab-7/myApp.py
+++ Lab-7/myApp.py
@@ -1,3 +1,48 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import sqlite3
+import hashlib
+import streamlit.components.v1 as components
 
-st.title("Hello")
+con = sqlite3.connect('data.db')
+cur = con.cursor()
+
+def main():
+    st.title("Movie Recommendation System")
+    menu = ["Home", "Sign-Up", "Login"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        home()
+
+    elif choice == "Sign-Up":
+        signup()
+
+    elif choice == "Login":
+        login()
+
+def signup():
+    st.title("Sign-Up Page")
+    st.write("This will have the sign-up for the app")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+    st.write("Terms and Conditions: ")
+    agree = st.checkbox("I Agree")
+    st.button("Sign-Up")
+
+def login():
+    st.title("Login Page")
+    st.write("This will be the login page for the app")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+    st.button("Login")
+
+def home():
+    st.title("Home Page")
+    st.write("This is the home page which will have the movie recommendations and search bar")
+    search_text = st.text_input("Search")
+    st.button("Search")
+
+if __name__ == "__main__":
+    main()
